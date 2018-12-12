ab5a6ea96744e4f6e11394d81d4a22298b0b151a
matthew woodward <matthew.woodward1982@gmail.com>
2018-12-12 13:18:48 -0700

    almost complete

--- assets/javascript/hikebike.js
+++ assets/javascript/hikebike.js
@@ -1,11 +1,42 @@
 $(document).ready(function () {
     //Declare API Key as a Global Variable
     var APIkey2 = "200393554-a94c289e68a08e7bdc65e6edd54c90f7";
+    let pos = {
+        allowed: false
+    };
+
+    async function getLocation() {
+        if (navigator.geolocation) {
+            let data = await navigator.geolocation.getCurrentPosition(showPosition)
+            return data;
+        }
+    }
+    function showPosition(position) {
+        let lat = position.coords.latitude.toFixed(0);
+        let lon = position.coords.longitude.toFixed(0);
+        pos = {
+            allowed: true,
+            lat,
+            lon
+        };
+    }
+    // checkbox "use current location/get location info"s
+
+    $('input[name="checkme"]').click(function () {
+        if ($(this).prop("checked") == true) {
+            getLocation();
+        }
+        else if ($(this).prop("checked") == false) {
+            console.log("Checkbox is unchecked.");
+        }
+    });
 
     //Hike button function
     $("#Hike").on("click", function () {
         event.preventDefault();
-        var zip = $("#zipInput").val();
+        let zip = $("#zipInput").val();
+
+
         var encodedURL = encodeURIComponent(
             "http://www.zipcodeapi.com/rest/gvvqqEnFXksoKlCTLC3V3NVtDJnDq3Ykq4GbH6sdm4FK9mS93BTXy9wKVP8McDbt/info.json/" +
             zip +
@@ -17,12 +48,19 @@ $(document).ready(function () {
             contentType: "application/json",
             url: "https://corsbridge2.herokuapp.com/" + encodedURL,
             success: function (response) {
+                let lat
+                let lon
                 console.log(encodedURL);
                 console.log(response.lat);
                 console.log(response.lng);
-
-                var lat = response.lat;
-                var lon = response.lng;
+                if ($('#checkme').prop('checked')) {
+                    lat = pos.lat;
+                    lon = pos.lon;
+                }
+                else {
+                    lat = response.lat;
+                    lon = response.lng;
+                }
                 var encodedURL2 = encodeURIComponent(
                     "https://www.hikingproject.com/data/get-trails?lat=" +
                     lat +
@@ -57,12 +95,20 @@ $(document).ready(function () {
                             return parseFloat(response.trails[i].stars);
                         });
                         console.log(ratingsArr);
-
+                        var sum = 0
+                        for (var i = 0; i < ratingsArr.length; i++) {
+                            sum += parseFloat(ratingsArr[i], 10);
+                        }
+                        var ratAvg = sum / ratingsArr.length;
+                        console.log("ravg: " + ratAvg);
                         //Display DATA
                         $("#maxDistance").html("Max Distance: " + maxDistance); //Can display a photo
+                        $("#minDistance").html("Min Distance: " + minDistance);
+                        $("#avgRating").html("Average Stars: " + ratAvg);
+                        $("#numResults").html("Number of Results:" + response.trails.length);
                         //$("#maxResults").append("<p>").text("Trails: " + response.trails[i].difficulty);
                         //$("#minLength").html("Trail Length: " + response.trails[i].length);
-                        //$("minStars").html("Rating: " + response.trails[i].stars);
+                        localStorage.setItem("response", JSON.stringify(response));
                     }
                 });
             }
@@ -84,13 +130,20 @@ $(document).ready(function () {
             contentType: "application/json",
             url: "https://corsbridge2.herokuapp.com/" + encodedURL,
             success: function (response) {
+                let lat
+                let lon
                 console.log(encodedURL);
                 console.log(response.lat);
                 console.log(response.lng);
-
-                var lat = response.lat;
-                var lon = response.lng;
-
+                if ($('#checkme').prop('checked')) {
+                    lat = pos.lat;
+                    lon = pos.lon;
+                }
+                else {
+                    lat = response.lat;
+                    lon = response.lng;
+                }
+                console.log("1- " + lat + "2- " + lon)
                 var encodedURL3 = encodeURIComponent(
                     "https://www.mtbproject.com/data/get-trails?lat=" +
                     lat +
@@ -125,11 +178,22 @@ $(document).ready(function () {
                             return parseFloat(response.trails[i].stars);
                         });
                         console.log("Ratings object: " + ratingsArr);
+                        var sum = 0
+                        for (var i = 0; i < ratingsArr.length; i++) {
+                            sum += parseFloat(ratingsArr[i], 10);
+                        }
+                        var ratAvg = sum / ratingsArr.length;
+                        console.log("ravg: " + ratAvg);
                         //Display DATA
-                        $("#maxDistance").html("Max Distance: " + maxDistance);
+                        $("#maxDistance").html("Max Distance: " + maxDistance); //Can display a photo
+                        $("#minDistance").html("Min Distance: " + minDistance);
+                        $("#avgRating").html("Average Stars: " + ratAvg);
+                        $("#numResults").html("Number of Results:" + response.trails.length);
+
                         //$("#maxResults").append("<p>").text("Trails: " + response.trails[j].difficulty);
                         //$("#minLength").html("Trail Length: " + response.trails[j].length);
                         //$("minStars").html("Rating: " + response.trails[i].stars);
+                        localStorage.setItem("response", JSON.stringify(response));
                     }
                 });
             }
---
+++ assets/javascript/trails.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    let response = JSON.parse(localStorage.getItem("response"));
+    localStorage.clear();
+
+    console.log(response);
+    for (i = 0; i < response.trails.length; i++) {
+        let trDiv = $("<div class='col-md-4'>");
+        let trName = $("<p>").text("Name: " + response.trails[i].name);
+        let trSumm = $("<p>").text("Summary: " + response.trails[i].summary);
+        let trDiff = $("<p>").text("Difficulty: " + response.trails[i].difficulty);
+        let trStars = $("<p>").text("Difficulty: " + response.trails[i].stars);
+        let trLink = $("<p>").text("Link: " + response.trails[i].url);
+
+        trDiv.append(trName);
+        trDiv.append(trSumm);
+        trDiv.append(trDiff);
+        trDiv.append(trStars);
+        trDiv.append(trLink);
+
+        $("#trailInfo").append(trDiv);
+    }
+
+});
